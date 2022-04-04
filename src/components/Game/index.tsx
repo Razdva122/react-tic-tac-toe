b6918590339c7e17d445a82b7969ac9bbe0099ba
Dmitrii <lexur-02@yandex.ru>
2022-04-04 22:54:45 +0500

    rewrite to func

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Square from '../Square';
 import RestartButton from '../RestartButton';
 
 import { initState, possibleWins } from './const';
-import type {TBoard, TCellValue, IApplicationState} from '../types';
+import type {TBoard, TCellValue} from '../types';
 
-class Board extends React.Component<{}, IApplicationState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = initState;
+function Board() {
+  const [state, updateState] = useState(initState);
+
+  function getValueOfCellFromBoard(x: number, y: number, board: TBoard) {
+    return board[y][x];
   }
 
-  clickOnSquare(x: number, y: number) {
-    if (this.state.winner !== '') {
+  function clickOnSquare(x: number, y: number) {
+    if (state.winner !== '') {
       return;
     }
 
-    if (this.getValueOfCellFromBoard(x, y, this.state.board) !== '') {
+    if (getValueOfCellFromBoard(x, y, state.board) !== '') {
       return;
     }
 
-    this.setState((prevState) => {
+    updateState((prevState) => {
       const prevBoard = prevState.board;
 
       const cloneBoard =[[...prevBoard[0]], [...prevBoard[1]], [...prevBoard[2]]];
@@ -35,10 +36,10 @@ class Board extends React.Component<{}, IApplicationState> {
       }
     });
 
-    this.setState((prevState) => {
+    updateState((prevState) => {
       let winner: TCellValue = '';
 
-      if (this.checkForEndGame(prevState.board)) {
+      if (checkForEndGame(prevState.board)) {
         winner = prevState.turn % 2 === 0 ? 'O' : 'X';
       }
 
@@ -49,13 +50,9 @@ class Board extends React.Component<{}, IApplicationState> {
     })
   }
 
-  getValueOfCellFromBoard(x: number, y: number, board: TBoard) {
-    return board[y][x];
-  }
-
-  checkForEndGame(board: TBoard): boolean {
+  function checkForEndGame(board: TBoard): boolean {
     return possibleWins.some((el) => {
-      const values = [0, 1, 2].map((index) => this.getValueOfCellFromBoard(el[index].y, el[index].x, board));
+      const values = [0, 1, 2].map((index) => getValueOfCellFromBoard(el[index].y, el[index].x, board));
 
       if (values[0] === '') {
         return false;
@@ -65,53 +62,51 @@ class Board extends React.Component<{}, IApplicationState> {
     })
   }
 
-  getStatusOfGame() {
+  function getStatusOfGame() {
     let status;
 
-    if (this.state.winner) {
-      status = `Game is Ended. Winner is ${this.state.winner}`;
-    } else if (this.state.turn === 9) {
+    if (state.winner) {
+      status = `Game is Ended. Winner is ${state.winner}`;
+    } else if (state.turn === 9) {
       status = `Game is Ended. Tie`;
     } else {
-      status = this.state.turn % 2 === 0 ? 'Next player: X' : 'Next player: O'
+      status = state.turn % 2 === 0 ? 'Next player: X' : 'Next player: O'
     }
 
     return status;
   }
 
-  isGameEnded() {
-    return this.state.winner !== '' || this.state.turn === 9;
+  function isGameEnded() {
+    return state.winner !== '' || state.turn === 9;
   }
 
-  restartGame() {
-    this.setState(initState);
+  function restartGame() {
+    updateState(initState);
   }
 
-  render() {
-    return (
-      <div>
-        <div className="status">{this.getStatusOfGame()}</div>
-        {
-          [[0, 1, 2], [3, 4, 5], [6, 7, 8]].map((row, y) => {
-            return (
-              <div className="board-row" key={y}>
-                {row.map((cell, x) => {
-                  return (
-                    <Square
-                      value={this.getValueOfCellFromBoard(x, y, this.state.board)}
-                      click={this.clickOnSquare.bind(this, x, y)}
-                      key={cell}
-                    />
-                  )
-                })}
-              </div>
-            )
-          })
-        }
-        <RestartButton display={this.isGameEnded()} restart={() => this.restartGame()}/>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <div className="status">{getStatusOfGame()}</div>
+      {
+        state.board.map((row, y) => {
+          return (
+            <div className="board-row" key={y}>
+              {row.map((value, x) => {
+                return (
+                  <Square
+                    value={value}
+                    click={clickOnSquare.bind(null, x, y)}
+                    key={x}
+                  />
+                )
+              })}
+            </div>
+          )
+        })
+      }
+      <RestartButton display={isGameEnded()} restart={restartGame}/>
+    </div>
+  );
 }
 
 const Game = () => (
--- src/components/RestartButton/index.tsx
+++ src/components/RestartButton/index.tsx
@@ -1,11 +1,11 @@
 export default function RestartButton({display, restart}: {display: boolean, restart: () => void}) {
-  if (display) {
-    return (
-      <button className="restart" onClick={restart}>
-        Restart Game
-      </button>
-    )
-  }
-
-  return null;
+  return(
+    <>
+        {display &&
+            <button className="restart" onClick={restart}>
+                Restart Game
+            </button>
+        }
+    </>
+  );
 }
